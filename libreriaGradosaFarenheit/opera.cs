71b48d0313aad3aa0db30af2917fdfe0bab83668
Jesus Pandura <jesmal.flex@hotmail.com>
2020-10-28 19:59:00 -0700

    Agregar archivos de proyecto.

---
+++ libreriaGradosaFarenheit/opera.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreriaGradosaFarenheit
+{
+    public class opera
+    {
+
+
+        public static double gradoscaf(double a)
+        {
+
+           double d;
+
+            d =  a * 1.8;
+            return d +32 ;
+           
+
+
+        }
+
+
+
+        public  static double  gradosfac(double a)
+        {
+              
+           
+            double d;
+            d = a -32 ;
+            return d / 1.8;
+
+
+        }
+
+
+
+
+    }
+
+   
+}
+
+
+
+
+   
